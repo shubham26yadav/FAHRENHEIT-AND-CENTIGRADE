6d5f7957cc764623fa3764df6e2f01501fcc7b9b
shubham yadav <115391493+shubham26yadav@users.noreply.github.com>
2022-10-09 12:50:23 +0530

    Add files via upload

---
+++ FAHRENHEIT AND CENTIGRADE.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	float c,f;
+	printf("Temperature in centigrade =");
+	scanf("%f",&c);
+	f=c*9/5+32;
+	printf("\nTemperature in farenheit =%f",f);
+return 0;
+}
+	
+	
+
+
+
